4f3ee65470714323b6e40180a4721a2663826ad2
Rener Pereira  de Castro <Rener.Castro@c6bank.com>
2018-10-09 15:24:15 -0300

    faltou :

--- gcloud-create-instance.sh
+++ gcloud-create-instance.sh
@@ -1,7 +1,6 @@
-
 #! /bin/bash
 
-while getopts p:n option
+while getopts p:n: option
 do
 case "${option}"
 in
@@ -10,12 +9,13 @@ n) NAME=${OPTARG};;
 esac
 done
 
-gcloud compute --project "$PROJ" \
+
+gcloud compute --project $PROJ \
 	instances create $NAME \
 	--zone "southamerica-east1-b" \
-	--machine-type "n1-standard-4" \
-	--image-project debian-cloud \
-	--image-family "debian-9" \
-	--create-disk "size=50GB" \
-	--deletion-protection \
-	--metadata-from-file startup-script="gcloud_install.sh"
+ 	--machine-type "n1-standard-4" \
+ 	--image-project debian-cloud \
+ 	--image-family "debian-9" \
+ 	--create-disk "size=50GB" \
+ 	--deletion-protection \
+ 	--metadata-from-file startup-script="gcloud_install.sh"
